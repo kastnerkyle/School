29577a145966b26c121e0ff69205036712b4bc7d
Kyle Kastner <kastnerkyle@gmail.com>
2012-04-25 21:18:14 -0500

    Split metropolis and metropolis hastings into two files

---
+++ ssp/homework3_metropolis.m
@@ -0,0 +1,35 @@
+%Metropolis sampling with 
+num_samples = 100000;
+burnin = 10000;
+in = [];
+out = [];
+
+%target is N(0,1), or normpdf(x, 0, 1);
+%init our mean at 0, and use an arbitrary std deviation of .35
+init = 1;
+chosen_var = .35;
+
+prev = 0;
+itr = 1;
+for i = 1:num_samples
+    candidate = prev + chosen_var*randn(1,1);
+    ll_prev = normpdf(prev, 0, 1);
+    ll_candidate = normpdf(candidate, 0, 1);
+    acceptance = min([ll_candidate/ll_prev, 1]);
+    in(i) = candidate;
+    if acceptance >= rand(1,1)
+        prev = candidate;
+        out(itr) = candidate;
+        itr = itr + 1;
+    end
+end
+
+%Use 1000 for better hist resolution
+[x1, n1] = hist(out(burnin:length(out)), 1000);
+figure();
+[x2, n2] = hist(in, 1000);
+
+plot(n1, x1, 'r');
+hold on;
+plot(n2, x2, 'b');
+
