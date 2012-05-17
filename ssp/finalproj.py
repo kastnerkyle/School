6b55fbc5028260ae738cf0b99c5627471b696d7e
Kyle Kastner <kastnerkyle@gmail.com>
2012-05-17 07:30:13 -0500

    Height still not working

--- ssp/finalproj.py
+++ ssp/finalproj.py
@@ -17,31 +17,56 @@ def update_var_est(samp_vec, mean_est):
 def update_mean_est(samp_vec, var_est):
     n = float(len(samp_vec))
     #Scale variable is standard deviation
-    return st.norm.rvs(np.mean(samp_vec), np.sqrt(var_est/n))
+    return st.norm.rvs(np.mean(samp_vec),
+                       scale=np.sqrt(var_est/n))
+
+def update_height_est(samp_vec, mean_est, var_est):
+    def update_height_mean(height_vec, height_var_est):
+        return update_mean_est(height_vec, height_var_est) 
+
+    def update_height_var(height_vec, height_mean_est):
+        return update_var_est(height_vec, height_mean_est)
+
+    height_mean_est = random.random()
+    height_var_est = random.random() 
+    run = 10
+    for i in range(run):
+        height_vec = [st.norm.pdf(x, height_mean_est, np.sqrt(height_var_est)) 
+                      for x in samp_vec]
+        height_mean_est = update_height_mean(height_vec, height_var_est)
+        height_var_est = update_var_est(height_vec, height_mean_est)
+    return height_mean_est
 
 def estimate(samp_vec):
     mean_est = random.random()
     var_est = random.random()
-    run = 10000
+    height_est = random.random()
+    run = 1
     for i in range(run):
         var_est = update_var_est(samp_vec, mean_est)
         mean_est = update_mean_est(samp_vec, var_est)
-    return ("ESTIMATED MEAN: " + `mean_est`, "ESTIMATED VAR:  " + `var_est`)
+        height_est = update_height_est(samp_vec, mean_est, var_est)
+        print "Iteration " + `i` + " complete"
+    return [("ESTIMATED MEAN: " + `mean_est`, "ESTIMATED VAR:  " + `var_est`, "ESTIMATED HEIGHT: " + `height_est`)]
 
 #means spaced from 1 to 100
 sample_count = 1000
-true_means = np.arange(180,1000,2*60)
+true_mean = 25
+true_var = 5
+true_height = 10
+samples_list = st.norm.rvs(true_mean,
+                           np.sqrt(true_var),
+                           size=sample_count)
 
-#variances randomly distributed around 40 with a variance of 20
-true_vars = 40+20*np.random.randn(np.size(true_means))
-samples_list = [x[0]+ np.sqrt(x[1])*np.random.randn(sample_count)
-                for x in zip(true_means, true_vars)]
+actuals = [("TRUE MEAN: " + `x[0]`,
+            "TRUE VAR: " + `x[1]`,
+            "TRUE HEIGHT: " + `x[2]`)
+            for x in [(true_mean, true_var, true_height)]] 
 
-actuals = [("TRUE MEAN: " + `x[0]`, "TRUE VAR: " + `x[1]`)
-           for x in zip(true_means, true_vars)] 
-
-p = Pool()
-estimates = p.map(estimate, samples_list)
+estimates = estimate(samples_list)
 for i,j in zip(estimates, actuals):
    print i
    print j
+
+plot.hist(samples_list)
+plot.show()
