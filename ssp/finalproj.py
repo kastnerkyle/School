39f283df5258a56a048f5985e5ef5a085dbbf356
Kyle Kastner <kastnerkyle@gmail.com>
2012-05-17 23:01:43 -0500

    Triple plot up and running

--- ssp/finalproj.py
+++ ssp/finalproj.py
@@ -15,16 +15,16 @@ class gaussian_peak:
 
 #means spaced from 1 to 100
 sample_count = 1000
-true_height = 5.
-true_mean = 10.
-true_var = .5
+true_heights = [5., 12., 6.]
+true_means = [10., 22., 6.]
+true_vars = [.5, .5, .5]
 
-n = gaussian_peak(true_height, true_mean, true_var)
-lb = .6*true_mean
-ub = 1.4*true_mean
+[n1, n2, n3] = [gaussian_peak(h, m, v) for h, m, v in zip(true_heights, true_means, true_vars)]
+lb = min(true_means) - 6*max(true_vars) 
+ub = max(true_means) + 6*max(true_vars)
 r = np.arange(lb, ub, (ub-lb)/100)
-plot.plot(r, [n.func(x) for x in r])
+plot.plot(r, [n1.func(x) for x in r], "b",
+          r, [n2.func(x) for x in r], "g",
+          r, [n3.func(x) for x in r], "r")
 plot.show()
 
-#plot.hist(samples_list)
-#plot.show()
