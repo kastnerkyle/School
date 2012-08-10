6e2041bfd8a8ca36fe54d0a30b6bccebc000e3c6
Kyle Kastner <kastnerkyle@gmail.com>
2012-08-10 00:22:53 -0500

    Working HTML parser

--- appgraph/finalproj/getLyrics.py
+++ appgraph/finalproj/getLyrics.py
@@ -4,6 +4,8 @@ import signal
 import PyQt4.QtCore as qtc
 import PyQt4.QtGui as qtg
 from PyQt4.QtWebKit import QWebPage as qwp
+import HTMLParser
+import re
 
 class Render(qwp):
     def __init__(self, url):
@@ -20,7 +22,42 @@ class Render(qwp):
         self.html = self.mainFrame().toHtml()
         self.app.quit()
 
+class ParseLyrics(HTMLParser.HTMLParser):
+    def __init__(self):
+       HTMLParser.HTMLParser.__init__(self)
+       self.recording = 0
+       self.data = []
+
+    def handle_starttag(self, tag, attributes):
+        if tag != "div":
+            return
+        if self.recording:
+           self.recording += 1
+        for name, value in attributes:
+            if name == "class" and value == "lyricbox":
+                break
+            else:
+                return
+        self.recording = 1
+    
+    def handle_endtag(self, tag):
+        if tag == "div" and self.recording:
+            self.recording -= 1
+
+    def handle_data(self, data):
+        if self.recording:
+            self.data.append(data)
+
 if __name__=="__main__":
     url = sys.argv[1]
     javascript_html = Render(url).html
-    print unicode(javascript_html).encode("utf-8")
+    final_html = unicode(javascript_html).encode("utf-8")
+    #Get approximate lyrics (all html inside lyricbox tag)
+    p = ParseLyrics()
+    p.feed(final_html)
+    #Remove unwanted newlines and tabs, and join as a giant string
+    unfiltered_lyrics = " ".join(filter(lambda x: x != "\t" and x != "\n", p.data))
+    #Split based on the ringtone tag using non-greedy matching
+    filtered_lyrics = re.split("Send.*?Cell", unfiltered_lyrics)
+    #Center of array should hold lyrics
+    print filtered_lyrics[1]
