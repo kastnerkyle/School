b62aa64aa594086d6f51dcc5f160423ef5e9b520
Kyle Kastner <kastnerkyle@gmail.com>
2012-08-15 07:26:17 -0500

    Modified buildGraph.py - should be working and saving the graph now

--- appgraph/finalproj/buildGraph.py
+++ appgraph/finalproj/buildGraph.py
@@ -3,19 +3,51 @@ import networkx as nx
 import glob
 distance_dir = "distances"
 songs_with_distances = glob.glob(distance_dir + "/*")
+import pickle 
 
-for sf in songs_with_distances:
-   try:
-       [source_band, source_song] = sf.split(":")
-   except ValueError:
-       print "Malformed song title " + sf + " skipping"
-       continue
-   f = open(sf)
-   for line in f:
-       [ngram_type, count] = line.split(",")[-2:]
-       match_band_and_song = line.split(",")[1]
-       try:
-          [match_band, match_song] = match_band_and_song.split(":")[0:2]
-       except ValueError:
-          print "Match song " + match_band_and_song_split + " malformed"
-   f.close()
+if __name__ == "__main__":
+    band_graph = nx.Graph()
+    band_song_graph = nx.Graph()
+    max_in_all_files = 0
+    for sf in songs_with_distances:
+        f = open(sf)
+        for line in f:
+            [ngram_type, count] = map(int, line.split(",")[-2:])
+            count = (ngram_type*count)*(ngram_type*5)
+            if count > max_in_all_files:
+               max_in_all_files = count
+        f.close()
+    print "Max value is " + str(max_in_all_files) + ", weights normalized by this value"
+        
+    for sf in songs_with_distances:
+        try:
+            [source_band, source_song] = sf.split(":")
+            source_band = source_band[len(distance_dir)+1:]
+        except ValueError:
+            source_band = sf[len(distance_dir)+1:]
+            source_song = "Unknown_Song"
+        f = open(sf)
+        for line in f:
+            [ngram_type, count] = map(int, line.split(",")[-2:])
+            match_band_and_song = "".join(line.split(",")[:-2])
+            count = (ngram_type*count)*(ngram_type*5)
+            try:
+               [match_band, match_song] = match_band_and_song.split(":")[0:2]
+            except ValueError:
+               match_band = match_band_and_song
+               match_song = "Unknown_Song"
+            fr = source_band + ":" + source_song
+            to = match_band + ":" + match_song
+            #Check for reverse edge since graph is undirected
+            if count > 0 and (to, fr) not in band_song_graph.edges() and (fr, to) not in band_song_graph.edges():
+                band_song_graph.add_edge(fr,to,weight = 1 - (float(count)/max_in_all_files))
+                #print "Added " + fr + "->" + to + " to the graph"
+            elif (to,fr) in band_song_graph.edges() and count > 0:
+                band_song_graph.edge[to][fr]["weight"] += 1 - (float(count)/max_in_all_files)
+                #print to + "->" + fr + " already in graph - adding"  
+                
+        f.close()
+        print "Finished " + sf
+    d = open("saved.graph", "w")
+    pickle.dump(band_song_graph, d)
+    d.close
