7d9a13f8b5890a6ce3b9711c55122708a5259289
Kyle Kastner <kastnerkyle@gmail.com>
2012-11-10 00:32:16 -0600

    First version with working VQ, need to add metropolis-hastings for sampling

--- dsp/vector_quant.py
+++ dsp/vector_quant.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python
-#sudo apt-get install python-scipy python-numpy
+#sudo apt-get install python-scipy python-numpy sndfile-programs sndfile-tools
 #http://www.voiptroubleshooter.com/open_speech/american.html
 #http://stackoverflow.com/questions/1545606/python-k-means-algorithm
 import scipy.io.wavfile as wavfile
+import numpy as np
 import sys
-
+import random
 sr, data = wavfile.read(sys.argv[1])
-print sr
-print len(data)
+
+means = [random.randrange(min(data),max(data),1) for i in range(256)]
+bound = 0.2
+
+data_to_quant = {}
+for p,d in enumerate(data):
+    smallest_error = None
+    for i,val in enumerate(means):
+        error = abs(d-val)
+        if error < smallest_error or smallest_error == None:
+            smallest_error = error
+            closest_k = i
+    data_to_quant[p] = closest_k
+    means[closest_k] = means[closest_k]*(1-bound) + d*bound
+
+out = []
+for i in data_to_quant.keys():
+    out.append(int(means[data_to_quant[i]]))
+
+wavfile.write("quantized.wav", sr, np.asarray(out).astype("int16"))
