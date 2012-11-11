d9d154defc4af7ce522b2d7d5e77baf9860e09d8
Kyle Kastner <kastnerkyle@gmail.com>
2012-11-10 19:59:49 -0600

    Added multiprocessing for slight boost in metropolis hastings

--- dsp/vector_quant.py
+++ dsp/vector_quant.py
@@ -9,12 +9,14 @@ import sys
 import random
 import argparse
 import operator
+from multiprocessing import Pool, cpu_count
 
 parser = argparse.ArgumentParser(description="Apply vector quantization using k-means clustering to a linearly encoded WAV file")
 parser.add_argument(dest="filename", help="WAV file to be processed")
 parser.add_argument("-b", "--bits", dest="bits", action="store", default=4, type=int, help="Integer number of bits used in quantized output, default is 4")
 parser.add_argument("-r", "--random", dest="random", action="store_true", help="Use uniform random algorithm for centroid initialization instead of Metropolis-Hastings")
-parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Show verbose output")
+parser.add_argument("-v", "--verbose", dest="verbose", action="count", help="Show verbose output")
+parser.add_argument("-s", "--single", dest="single", action="store_true", help="Single core mode for Metropolis Hastings algorithm")
 
 try:
     args = parser.parse_args()
@@ -25,7 +27,7 @@ except SystemExit:
 sr, data = wavfile.read(args.filename)
 centroid_count = 2**args.bits
 
-if args.verbose:
+if args.verbose > 0:
     print "File has a sample rate of " + `sr` + " samples per second"
     print "File has " + `len(data)` + " values, " + `len(data)/float(sr)` + " seconds"
     print "Vector quantization will use " + `centroid_count` + " centroids for k-means calculations"
@@ -39,34 +41,61 @@ if args.random:
         print "Centroid calculations complete"
 else:
     centroid_type = "metropolis_hastings"
-    if args.verbose:
+    if args.verbose > 0:
         print "Using Metropolis-Hastings algorithm for initial centroid distribution"
-    start = data[random.randrange(0,len(data),1)]
-    std_dev = np.std(data) #arbitrary std_dev
-    means = []
-    prev = start
-    while len(means) < centroid_count:
-        if args.verbose:
-            print "Currently " + `len(means)` + " centroids calculated"
-        candidate = prev + std_dev * np.random.randn()
-        prob_prev = len(filter(lambda x: x < prev, data))/float(len(data))
-        prob_candidate = len(filter(lambda x: x < candidate, data))/float(len(data))
-        acceptance = prob_candidate/prob_prev
-        if acceptance >= 1:
-            prev = candidate
-            means.append(candidate)
-        else:
-            if random.random() <= acceptance:
+
+    if args.single:
+        split=1
+    else:
+        split = cpu_count()
+
+    if args.verbose > 0:
+        print "Using " + `split` + " cores for the Metropolis Hastings calculations"
+
+    def metropolis_run(data):
+        start = data[random.randrange(0,len(data),1)]
+        std_dev = np.std(data) #arbitrary std_dev larger value will give better centroid spread
+        means = []
+        prev = start
+        while len(means) < centroid_count/split:
+            if args.verbose > 1:
+                print "Currently " + `len(means)` + " centroids calculated"
+            candidate = prev + std_dev * np.random.randn()
+            prob_prev = len(filter(lambda x: x < prev, data))/float(len(data))
+            prob_candidate = len(filter(lambda x: x < candidate, data))/float(len(data))
+            acceptance = prob_candidate/prob_prev
+            if acceptance >= 1:
                 prev = candidate
                 means.append(candidate)
-    if args.verbose:
+            else:
+                if random.random() <= acceptance:
+                    prev = candidate
+                    means.append(candidate)
+        return means
+
+    p = Pool(split)
+    centroids = p.map_async(metropolis_run, [data]*split)
+    p.close()
+    means = []
+    for group in centroids.get():
+        means.extend(group)
+
+    if len(means) != centroid_count:
+        print "Split value (number of available cores) must be evenly divisible into 2^bits. Use the --single or -s option!"
+        sys.exit()
+
+    if args.verbose > 1:
+        print "Means initialized at: "
+        print means
+
+    if args.verbose > 0:
         print "Centroid calculation complete"
 
 bound = 0.1 #arbitrary bound for mean movement, must be between 0 and 1, higher values allow centroids to move more
 
 data_to_quant = {}
 for i,d in enumerate(data):
-    if args.verbose:
+    if args.verbose > 1 and i%10000 == 0:
         print "Processing sample " + `i` + " of " + `len(data)`
     smallest_error = None
     for n,v in enumerate(means):
@@ -81,6 +110,6 @@ out = np.zeros(len(data))
 for k in data_to_quant.keys():
     out[k] = int(means[data_to_quant[k]])
 
-wavfile.write("quantized_" + centroid_type + "_" + args.filename, sr, np.asarray(out).astype("int16"))
+wavfile.write("vector_quantized_" + centroid_type + "_" + args.filename, sr, np.asarray(out).astype("int16"))
 
 print "Processing complete for " + args.filename
