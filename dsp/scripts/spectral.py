b118c804a3981906d259d7fe196d770b8ead5fae
Kyle Kastner <kkastner@MacBookAir.(none)>
2012-11-28 22:08:27 -0600

    Modified spectral, about to try to get 3d plot then verify correctness of output

--- dsp/scripts/spectral.py
+++ dsp/scripts/spectral.py
@@ -8,13 +8,14 @@ import sys
 import matplotlib.pyplot as plot
 from numpy.lib.stride_tricks import as_strided as ast
 import copy
+from mpl_toolkits.mplot3d import Axes3D
 
 parser = argparse.ArgumentParser(description="Apply ")
 parser.add_argument(dest="filename", help="WAV file to be processed")
 parser.add_argument("-n", "--noplot", dest="noplot", action="store_true", help="WAV file to be processed")
 parser.add_argument("-v", "--verbose", dest="verbose", action="count", help="Show verbose output, use -vv for highest verbosity")
 parser.add_argument("-s", "--specgram", dest="specgram", action="store_true", help="Show spectrogram of input wavefile")
-parser.add_argument("-c", "--scf", dest="scf", action="store_true", help="Run spectral correlation function against input")
+parser.add_argument("-c", "--coherence", dest="coherence", action="store_true", help="Plot cyclic coherence of input")
 parser.add_argument("-f", "--fft", type=int, dest="fft", action="store", default=1024, help="Optionally set FFT size, default is 1024")
 
 def run_specgram():
@@ -51,28 +52,70 @@ def run_specgram():
             print "Exiting, spectrogram complete!"
             sys.exit()
 
-def run_spectral_correlation():
+def alpha_run(d1, d2, alpha, beta):
+    if d1.shape != d2.shape:
+        print "Items to be correlated do not have the same dimensions!"
+        sys.exit()
+    vec_len, = d1.shape
+    spacing = d1.itemsize
+    block = args.fft/2
+    #Need 67% overlap for SCF using Hann window according to Cyclic Spectral Analysis in Practice - Jerome Antoni
+    increment = int(block*.33)
+    #Add extra zeros for tail end in order to avoid junk data - must keep dtype as int16 - otherwise weird broadcasting rules will take over...
+    #http://www.utc.fr/~antoni/programm.htm
+    d1 = np.hstack((d1, np.zeros(block, dtype=np.complex64)))
+    d2 = np.hstack((d2, np.zeros(block, dtype=np.complex64)))
+    t = np.arange(0,d1.shape[0])
+    plus_alpha = np.asarray([np.exp(1j*np.pi*alpha*beta*n) for n in t], dtype=np.complex64)
+    minus_alpha = np.asarray([np.exp(-1j*np.pi*alpha*beta*n) for n in t], dtype=np.complex64)
+    x = d1*minus_alpha
+    y = d2*plus_alpha
+    def overlap(d):
+        return ast(d, shape=(vec_len/increment,block), strides=(increment*spacing, spacing))
+    window = np.hanning(block)
+    Xconj = np.conjugate(np.fft.fftn(overlap(x)*window, s=(args.fft,), axes=(1,)))
+    Y = np.fft.fftn(overlap(y)*window, s=(args.fft,), axes=(1,))
+    normalizing_factor = Y.shape[0]*np.linalg.norm(window)**2
+    return np.sum(Xconj*Y, axis=0, dtype=np.complex64)/float(normalizing_factor)
+
+def run_cyclic_coherence():
     sr, data = wavfile.read(args.filename)
-    data = np.asarray(data, dtype=np.complex64)
-    alpha = .5
-
-    def alpha_run(data, alpha):
-        vec_len, = data.shape
-        spacing = data.itemsize
-        window = args.fft
-        #Need 67% overlap for SCF using Hann window according to Cyclic Spectral Analysis in Practice - Jerome Antoni
-        increment = int(window*.33)
-        #Add extra zeros for tail end in order to avoid junk data - must keep dtype as int16 - otherwise weird broadcasting rules will take over...
-        #http://www.utc.fr/~antoni/programm.htm
-        data = np.hstack((data, np.zeros(window, dtype=np.complex64)))
+    data = np.asarray(data[0:1000], dtype=np.complex64)
+    coh = None
+    start_alpha = 1
+    stop_alpha = 800
+    step_alpha = 1
+    alphas = range(start_alpha, stop_alpha, step_alpha)
+    alphas = [ 1./x if x !=0 else None for x in alphas ]
+    alphas = filter(lambda x: x != None, alphas)
+    #http://stackoverflow.com/questions/9152958/matplotlib-3d-plot-2d-format-for-input-data
+    beta = .5
+    max_step = 1./data.shape[0]
+    yaxis = np.arange(0,args.fft)
+    xaxis = alphas
+    xaxis = np.asarray(xaxis)
+    for n,alpha in enumerate(xaxis):
+        if args.verbose > 0:
+            print "Running alpha " + `1./alpha`
         t = np.arange(0,data.shape[0])
-        plus_alpha = np.asarray([np.exp(1j*np.pi*alpha*n) for n in t], dtype=np.complex64)
-        minus_alpha = np.asarray([np.exp(-1j*np.pi*alpha*n) for n in t], dtype=np.complex64)
-        return ast(data, shape=(vec_len/increment,window), strides=(increment*spacing, spacing))
-
-    overlapped_data = alpha_run(data, alpha)
-    fft = np.fft.fftn(overlapped_data, s=(args.fft,), axes=(1,))
-    plot.imshow(abs(fft))
+        plus_alpha = np.asarray([np.exp(1j*np.pi*alpha*beta*k) for k in t], dtype=np.complex64)
+        minus_alpha = np.asarray([np.exp(-1j*np.pi*alpha*beta*k) for k in t], dtype=np.complex64)
+        y = data*minus_alpha
+        x = data*plus_alpha
+        xx = alpha_run(x, x, 0, beta)
+        yx = alpha_run(y, x, 0, beta)
+        #xy = alpha_run(x, y, alpha, beta)
+        yy = alpha_run(y, y, 0, beta)
+        res = yx/np.sqrt(yy*xx)
+        if coh == None:
+            coh = np.zeros((xaxis.shape[0],args.fft), dtype=np.complex64)
+        coh[n,:] = res
+    X, Y = np.meshgrid(xaxis, yaxis)
+    Z = coh
+    #f = plot.figure()
+    #ax = Axes3D(f)
+    #ax.plot(X, Y, Z)
+    plot.imshow(abs(Z))
     plot.show()
 try:
     args = parser.parse_args()
@@ -82,5 +125,5 @@ except SystemExit:
 
 if args.specgram:
     run_specgram()
-elif args.scf:
-    run_spectral_correlation()
+elif args.coherence:
+    run_cyclic_coherence()
