eafdacf41fd6f1e7394c5a7bc1000f64f4693672
Kyle Kastner <kkastner@kkastner-MacBookAir.(none)>
2013-02-13 01:13:45 -0600

    Semi working with noise...

--- atpr/linear_regression.py
+++ atpr/linear_regression.py
@@ -2,7 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plot
 import math
-import sys
 
 def gen_dft(m,n,N):
     return np.exp(-1j*2*math.pi*m*n/N)
@@ -14,15 +13,17 @@ def gen_gaussian(m,n):
         return np.exp(-(m**2)/(float(n)**2))
 
 N = 10
+noise_var = B = .000000000000000001
 #Calculate using the Penrose-Moore Pseudo Inverse
 PMSI = False
 data = w = np.matrix(np.random.randn(N,1))
 basis = np.matrix(np.zeros((N,N)))
 for m in range(N):
     for n in range(N):
-        basis[m,n] = gen_gaussian(m,n)
-        #basis[m,n] = gen_dft(m,n,N)
-output = t = basis*w
+        #basis[m,n] = gen_gaussian(m,n)
+        basis[m,n] = gen_dft(m,n,N)
+noise = (B**2)*np.random.randn(N,1)
+output = t = basis*w + (B**2)*np.random.randn(N,1)
 if basis.shape[0] != basis.shape[1] or PMSI == True:
     maximum_likelihood = wml = np.linalg.inv(basis.conj().T*basis)*basis.conj().T*t
 else:
