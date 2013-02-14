db9ce7a252e0cdbbbab1444c17ad74daed7dca17
Kyle Kastner <kkastner@kkastner-MacBookAir.(none)>
2013-02-13 19:11:41 -0600

    Updated with sine wav data as core

--- atpr/linear_regression.py
+++ atpr/linear_regression.py
@@ -12,29 +12,28 @@ def gen_gaussian(x,m,n):
     else:
         return np.exp(-(np.abs(x-m)**2)/(2*float(n)**2))
 
-N = 10
-noise_var = B = .8
+N = 50
+noise_var = B = 1
 #Calculate using the Moore-Penrose pseudoinverse
-MPSI = False
-data = w = np.matrix(np.random.randn(N,1))
-w += np.sqrt(B)*np.random.randn(N,1)
+MPSI = True
+#data = np.matrix([np.sin(x)+2 for x in np.linspace(-np.pi,np.pi,N)]).T
+data = np.matrix(np.random.randn(N,1))
+w = data + np.sqrt(B)*np.random.randn(N,1)
 basis = np.matrix(np.zeros((N,N)))
 for m in range(N):
     for n in range(N):
         basis[m,n] = gen_gaussian(w[n],m,n)
         #basis[m,n] = gen_dft(m,n,N)
-test_data = q = w + np.sqrt(B)*np.random.randn(N,1)
-t = basis*(q)
+test_data = q = data + np.sqrt(B)*np.random.randn(N,1)
+t = basis*q
 use_MPSI = basis.shape[0] != basis.shape[1] or MPSI == True
 if use_MPSI:
-    maximum_likelihood = wml = np.linalg.inv(basis.T*basis)*basis.T*t
+    #Direct calculation appears to have numerical instability issues...
+    #maximum_likelihood = wml = np.linalg.inv(basis.T*basis)*basis.T*t
+    maximum_likelihood = wml = np.linalg.pinv(basis)*t
 else:
     maximum_likelihood = wml = np.linalg.inv(basis)*t
-f, axarr = plot.subplots(3)
-axarr[0].plot(np.real(w))
-axarr[0].set_title("Training data with $\\beta=$"+`noise_var`+" variance additive noise")
-axarr[1].plot(np.real(q))
-axarr[1].set_title("Test data with $\\beta=$"+`noise_var`+" variance additive noise")
-axarr[2].plot(np.real(wml))
-axarr[2].set_title("Recovered data using " + ("Penrose-Moore pseudoinverse" if use_MPSI else "matrix inverse"))
+plot.plot(np.real(w), 'b')
+plot.plot(np.real(q), 'r--')
+plot.plot(np.real(wml), 'g:')
 plot.show()
