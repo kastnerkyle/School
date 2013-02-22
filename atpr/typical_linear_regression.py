38e9b3bda449ca0618921b472f91de7d765d2a4c
Kyle Kastner <kkastner@kkastner-MacBookAir.(none)>
2013-02-21 21:13:15 -0600

    Fixed linear regression to be actually working now... at least with polynomial basis

--- atpr/typical_linear_regression.py
+++ atpr/typical_linear_regression.py
@@ -6,38 +6,37 @@ import math
 def gen_dft(m,n,N):
     return np.exp(-1j*2*math.pi*m*n/N)
 
-def gen_gaussian(x,m,n):
-    if n == 0:
-        return 1.0
-    else:
-        return np.exp(-(np.abs(x-m)**2)/(2*float(n)**2))
+def gen_polynomial(x, m):
+    return x**m
 
-N = 25
-noise_var = B = .1
+N = 100
+N_basis = 6
+noise_var = B = .2
 #Calculate using the Moore-Penrose pseudoinverse
 MPSI = True
-#Create a sinewave with freq cycles + dc offset to create a wave from 1 3 rather than -1 to 1
-freq = 4
-data = np.matrix([np.sin(x) for x in np.linspace(-freq*np.pi,freq*np.pi,N)]).T
-#data = np.matrix(np.random.randn(N,1))
-w = data + np.sqrt(B)*np.random.randn(N,1)
-basis = np.matrix(np.zeros((N,N)))
-for m in range(N):
+basis = np.matrix(np.zeros((N,N)), dtype=np.complex64)
+xs = np.matrix(np.arange(N)/float(N)).T
+freq = 6
+ys = np.sin(freq*2*np.pi*xs)
+w = ys + np.sqrt(B)*np.random.randn(N,1)
+for m in range(N_basis):
     for n in range(N):
-        basis[m,n] = gen_gaussian(w[n],m,n)
-        #asis[m,n] = gen_dft(m,n,N)
-test_data = t = data + np.sqrt(B)*np.random.randn(N,1)
-#t = basis*t
+        if n == 0:
+            basis[m,n] = 1.0
+        else:
+            #basis[m,n] = gen_dft(m,n,N)
+            basis[m,n] = gen_polynomial(w[n], m)
+
+test_data = t = basis*w
 use_MPSI = (basis.shape[0] != basis.shape[1] or MPSI == True)
 if use_MPSI:
     #Direct calculation appears to have numerical instability issues...
-    maximum_likelihood = wml = np.linalg.inv(basis.T*basis)*basis.T*t
-    #maximum_likelihood = wml = np.linalg.pinv(basis)*t
+    #maximum_likelihood = wml = np.linalg.inv(basis.T*basis)*basis.T*t
+    maximum_likelihood = wml = np.linalg.pinv(basis)*t
 else:
     maximum_likelihood = wml = np.linalg.inv(basis)*t
-f, axarr = plot.subplots(4)
-axarr[0].plot(data, 'k')
-axarr[1].plot(np.real(w), 'b')
-axarr[2].plot(np.real(t), 'r')
-axarr[3].plot(np.real(wml), 'g')
+plot.figure()
+plot.plot(ys, 'k')
+#plot.plot(w, 'b')
+plot.plot(np.real(wml), 'g')
 plot.show()
--- atpr/unknown_variance.py
+++ atpr/unknown_variance.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python
 import numpy as np
 import matplotlib.pyplot as plot
-total_obs = 20000
-obs_granularity = N = 10
-primary_mean = 5
+total_obs = 2000
+primary_mean = known_mean = 5
 primary_var = 14
 x = np.sqrt(primary_var)*np.random.randn(total_obs) + primary_mean
-group = x[:N]
 all_a = []
 all_b = []
-all_meanprec = []
-all_varprec = []
-prior_a=N/2.+1
-prior_b=1/2.*np.sum((group-primary_mean)**2)
-plot.figure()
-plot.plot(x)
-for i in range(1,int(float(total_obs)/N)-1):
-    group=x[N*i:N*(i+1)]
-    posterior_a=prior_a+N/2.
-    posterior_b=prior_b+1/2.*np.sum((group-primary_mean)**2)
+all_prec_guess = []
+all_prec_doubt = []
+prior_a=1/2.+1
+prior_b=1/2.*np.sum((x[0]-primary_mean)**2)
+f,axarr = plot.subplots(3)
+axarr[0].plot(x)
+for i in range(1,total_obs):
+    posterior_a=prior_a+1/2.
+    posterior_b=prior_b+1/2.*np.sum((x[i]-known_mean)**2)
     all_a.append(posterior_a)
     all_b.append(posterior_b)
-    all_meanprec.append(posterior_a/posterior_b)
-    all_varprec.append(posterior_a/(posterior_b**2))
+    all_prec_guess.append(posterior_a/posterior_b)
+    all_prec_doubt.append(posterior_a/(posterior_b**2))
     prior_a=posterior_a
     prior_b=posterior_b
-
-plot.figure()
-print "Estimated variance is " + `1./all_meanprec[-1]`
-print "Confidence in variance estimation is " + `1-all_varprec[-1]`
-plot.plot(all_meanprec)
-plot.figure()
-plot.plot(all_varprec)
+axarr[1].plot(all_prec_guess)
+axarr[2].plot(all_prec_doubt)
 plot.show()
