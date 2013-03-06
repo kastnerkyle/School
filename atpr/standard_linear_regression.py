4a07b153fbb0cc49532bfea3a9fe4f4f8d8eb61d
Kyle Kastner <kkastner@kkastner-MacBookAir.(none)>
2013-03-05 23:04:14 -0600

    Made extensive modifications to linear regression code and get a semi working version now

--- atpr/standard_linear_regression.py
+++ atpr/standard_linear_regression.py
@@ -3,33 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plot
 import math
 
-def gen_dft(m,n,N):
-    return np.exp(-1j*2*math.pi*m*n/N)
+def gen_dft(m, n, N):
+    return np.exp(1j*-2*m*n/N)
 
 def gen_polynomial(x, m):
     return x**m
 
-N = 100
-#N_basis works well at low values for polynomial regression but dft regression needs more...
-N_basis = 6
-noise_var = B = .2
-
+N = 10
+N_basis = 3
+noise_var = B = 5
 basis = np.matrix(np.zeros((N,N)), dtype=np.complex64)
-xs = np.matrix(np.arange(N)/float(N)).T
-num_cycles = 6
-ys = np.sin(num_cycles*2*np.pi*xs)
+xs = np.matrix(range(N)).T
+ys = np.square(xs) - 4*xs + 1
+
 w = ys + np.sqrt(B)*np.random.randn(N,1)
-#Trained on model data rather than measured...
 for m in range(N_basis):
     for n in range(N):
-        if n == 0:
+        if m == 0:
             basis[m,n] = 1.0
         else:
-            #basis[m,n] = gen_dft(m,n,N)
-            basis[m,n] = gen_polynomial(w[n], m)
+            basis[m,n] = gen_polynomial(xs[n], m)
+            #To use the gen_dft basis, make sure to set N_basis = N
+            #basis[m,n] = gen_dft(m, n, N)
 
-test_data = t = basis*w #+ np.sqrt(B)*np.random.randn(N,1))
-
+test_data = t = basis*w
 #Calculate using the Moore-Penrose pseudoinverse using the following formula
 #maximum_likelihood = wml = np.linalg.inv(basis.T*basis)*basis.T*t
 #Direct calculation appears to have numerical instability issues...
@@ -37,7 +34,8 @@ test_data = t = basis*w #+ np.sqrt(B)*np.random.randn(N,1))
 maximum_likelihood = wml = np.linalg.pinv(basis)*t
 
 plot.figure()
-plot.plot(ys, 'k')
-plot.plot(w, 'b')
+plot.title("Regression fit using polynomial basis function, number of basis functions = $" + `N_basis` + "$")
+plot.plot(ys, 'b')
+plot.plot(w, 'ro')
 plot.plot(np.real(wml), 'g')
 plot.show()
